c9603a9c6af05189b4e38362472e7b1e66058fd7
Franklin-Kimatu <frankimatu@gmail.com>
2019-07-19 22:24:47 +0300

    This commit will make the design and development icons toggle

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#design").click(function(){
+        $("#design").hide();
+        $("#designdescription").show();
+    });
+    $("#designdescription").click(function(){
+        $("#designdescription").hide();
+        $("#design").show();
+    });
+    $("#development").click(function(){
+        $("#development").hide();
+        $("#developmentdescription").show();
+    });
+    $("#developmentdescription").click(function(){
+        $("#developmentdescription").hide();
+        $("#development").show();
+    });
+    $("#product").click(function(){
+        $("")
+    })
+})
